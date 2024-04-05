9e62a724119588a31a684f5dee966abe0d610e49
Danny Fong <danny.fong.dev@outlook.com>
2024-04-04 19:14:03 -0700

    refactor GameToken, svg

--- src/components/GameToken.vue
+++ src/components/GameToken.vue
@@ -1,16 +1,10 @@
 <template>
-  <div
+  <!-- <div
     class="bg-transparent"
     :class="[winner === choice ? 'animate-bounce' : '']"
-  >
-    <div class="rounded-full p-4" :class="backgroundGradient" v-show="choice">
-      <img
-        class="w-24 bg-white rounded-full"
-        :src="imageURL"
-        alt="game token"
-      />
-    </div>
-  </div>
+  > -->
+  <img :src="imageURL" alt="game token" class="w-32" />
+  <!-- </div> -->
 </template>
 
 <script>
