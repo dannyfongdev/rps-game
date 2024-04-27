033903f0417d89e14b9b7113ff2d0f4d6d2d1761
Danny Fong <danny.fong.dev@outlook.com>
2024-04-26 20:52:06 -0700

    undoe margin

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex justify-center mb-1">
+  <div class="flex justify-center">
     <button
       @click="handleClick"
       class="mx-auto border-2 px-10 py-2 border-header-outline rounded-lg lg:w-[128px] lg:h-[40px] lg:absolute lg:right-[30px] lg:bottom-[30px] lg:py-1"
